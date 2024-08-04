e6bbb99499fe2216978e8c83384fc137a4867430
yangwei <yangwei@csair.com>
2024-08-04 14:03:40 +0800

    Adjust Volume

---
+++ PomodoroTimer/PomodoroTimer/Converters/IntegerConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace PomodoroTimer.Converters
+{
+    public class IntegerConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Math.Floor((double)value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new InvalidOperationException("Conversion not supported");
+        }
+    }
+}
